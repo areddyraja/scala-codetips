f7f2efcdd6c98d61256c6398ee1bebd04bce2dfb
areddyraja <areddyraja@gmail.com>
2016-03-19 14:44:29 +0530

    Added configuration system for the Scala & Akka stuff

---
+++ src/main/scala/com/akantha/config/AppConfig.scala
@@ -0,0 +1,24 @@
+package com.akantha.config
+
+import java.io.File
+import com.typesafe.config.ConfigFactory
+import com.typesafe.config.Config
+
+/**
+ * @author reddyraja
+ */
+trait AppConfig extends Configuration {
+
+  /**
+   *
+   */
+  def configFile: File
+
+  var _config: Config = ConfigFactory parseFile configFile
+
+  /**
+   * The holder for all the application specific properties.
+   */
+  def config: Config = _config
+
+}
---
+++ src/main/scala/com/akantha/config/MyAppConfig.scala
@@ -0,0 +1,15 @@
+package com.akantha.config
+
+import java.io.File
+
+/**
+ * @author reddyraja
+ */
+object MyAppConfig extends AppConfig{
+  def configFile: File = new File(sys.props("user.dir") + java.io.File.separator + "conf" + java.io.File.separator + "appconfig.conf")
+  def main( args:Array[String] ):Unit = {
+    args foreach println
+    println(sys.props("user.dir"))
+    println(MyAppConfig.get("sankri_server.interface", "default"))
+  } 
+}
